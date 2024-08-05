11f6290af16f1de010e44d9b2b4f6bf8498f8c36
Puneet chauhan <pc3622433@gmail.com>
2024-08-05 10:32:02 +0530

    strings

---
+++ string.py/slicingstrings.py
@@ -0,0 +1,12 @@
+a = "Shiv Shankar Ka Bhajan Karya kar"
+print(a)
+
+# print(a[0:4]) 
+
+# print(a[15:23])
+
+print(a[-4:])
+
+# slicing([start:end:step])
+b = '8578903'
+print(b[::2])
---
+++ string.py/stringsmethods.py
@@ -0,0 +1,28 @@
+a = "Hello world"
+
+# print(len(a))
+
+# print(a.count("e"))
+
+# print(a.upper())
+# print(a.lower())
+
+# print(a.index("W"))
+# print(a.index(" "))
+
+# print(a.capitalize())
+
+b = "john"
+
+# c = "my name is {} and my father name is {}"
+
+# print(c.format(b , "rohn"))
+ 
+print(b.center(10 , "_"))  # center(kitne se , aur space symbol)
+
+
+# print(a.find("r"))
+
+c = "puneet chauhan is a good  boy"
+print(c)
+print(c.replace("good" , "bad"))
