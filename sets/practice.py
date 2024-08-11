507ef85b6780abc013e2aef941d358ebeb417683
Puneet chauhan <pc3622433@gmail.com>
2024-08-11 11:09:04 +0530

    set

---
+++ sets/practice.py
@@ -0,0 +1,22 @@
+#  max and min
+
+# a = {20,25,30,35,40}
+# print(type(a))
+
+# print(max(a))  #max in 
+# print(min(a))  #min in 
+
+a ={"puneet","Chauhan","Khushi","Jatin","Ritesh","Ruchika"}
+b ={"Shiv","maa Sati","PArvati"}
+c ={"puneet","Khushi","panch veer"}
+
+
+# a.intersection_update(c)
+# print(a)
+
+a.remove("Chauhan")
+print(a)
+# if "Chauhan" in a:
+#     a.remove("Chauhan")
+# print(a)
+   
